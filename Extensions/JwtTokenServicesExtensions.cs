82dca429fc31f280dfee97fd005b7daf444304d8
Aleksander Zverev <aleksander.zverev.lt@gmail.com>
2022-02-06 10:00:50 +0500

    Add jwt authorization

---
+++ Extensions/JwtTokenServicesExtensions.cs
@@ -0,0 +1,41 @@
+﻿using IntervalLearningApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace IntervalLearningApi.Extensions;
+
+public static class JwtTokenServicesExtensions
+{
+    public static void AddJwtTokenServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var bindJwtSettings = new JwtSettings();
+
+        configuration.Bind("JsonWebTokenKeys", bindJwtSettings);
+
+        services.AddSingleton(bindJwtSettings);
+
+        services.AddAuthentication(options => 
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options => {
+            //TODO: not safe
+            options.RequireHttpsMetadata = false;
+
+            options.SaveToken = true;
+
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = bindJwtSettings.ValidateIssuerSigningKey,
+                IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(bindJwtSettings.IssuerSigningKey)),
+                ValidateIssuer = bindJwtSettings.ValidateIssuer,
+                ValidIssuer = bindJwtSettings.ValidIssuer,
+                ValidateAudience = bindJwtSettings.ValidateAudience,
+                ValidAudience = bindJwtSettings.ValidAudience,
+                RequireExpirationTime = bindJwtSettings.RequireExpirationTime,
+                ValidateLifetime = bindJwtSettings.RequireExpirationTime,
+                ClockSkew = TimeSpan.FromDays(1),
+            };
+        });
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,43 @@
+using IntervalLearningApi.Extensions;
+using IntervalLearningApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JsonWebTokenKeys"));
+
 builder.Services.AddCors();
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
+builder.Services.AddJwtTokenServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -12,8 +45,15 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseCors(c => c.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(options =>
+    {
+        options.RouteTemplate = "api/swagger/{documentName}/swagger.json";
+    });
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/api/swagger/v1/swagger.json", "My Cool API V1");
+        options.RoutePrefix = "api/swagger";
+    });
 }
 else
 {
@@ -24,6 +64,9 @@ else
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 
 app.MapControllers();
