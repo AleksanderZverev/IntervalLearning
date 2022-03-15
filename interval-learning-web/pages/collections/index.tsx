1ce877f9fffeb427f2ba1a99875aceac34860b6b
Aleksander Zverev <aleksander.zverev.lt@gmail.com>
2022-03-15 17:23:55 +0500

    >collection modal add start logic

--- interval-learning-web/pages/collections/index.tsx
+++ interval-learning-web/pages/collections/index.tsx
@@ -1,24 +1,36 @@
-import { Container } from '@mui/material';
+import { Button, CircularProgress, Container } from '@mui/material';
 import { FC, useEffect, useLayoutEffect, useState } from 'react';
+import { CreateCollectionModal } from '../../src/controls/Modals/CreateCollectionModal';
+import { withQueryResolver } from '../../src/hoc/withQueryResolver';
 import { useGetCollectionsQuery } from '../../src/redux/collectionSlice';
+import { Collection } from '../../src/types/Collection';
+import style from './collections.module.css';
 
-const CollectionsPage: FC = () => {
-    const { data: collections, isFetching, isError } = useGetCollectionsQuery();
-
-    if (isFetching) return <div>LOADING...</div>;
-    if (isError) return <div>ERROR...</div>;
+const CollectionsPageContent: FC<{ data: Collection[] }> = ({ data: collections }) => {
+    const [showCreateCollectionModal, setShowCreateCollectionModal] = useState(false);
 
     return (
         <div>
-            <div>Collections page</div>
+            {showCreateCollectionModal && (
+                <CreateCollectionModal
+                    open={showCreateCollectionModal}
+                    onClose={() => setShowCreateCollectionModal(false)}
+                />
+            )}
+            <div className={style.headerContainer}>
+                <h2 className={style.header}>Collections page</h2>
+                <Button onClick={() => setShowCreateCollectionModal(true)}>Создать коллекцию</Button>
+            </div>
             <div>
-                {collections?.map((c) => (
+                {collections.map((c) => (
                     <div key={c.id}>{c.title}</div>
                 ))}
             </div>
         </div>
     );
 };
+
+const CollectionsPage = withQueryResolver(useGetCollectionsQuery)(CollectionsPageContent);
 
 (CollectionsPage as any).auth = true;
 
---
+++ interval-learning-web/src/controls/Modals/CreateCollectionModal.tsx
@@ -0,0 +1,49 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Input, Modal, TextField } from '@mui/material';
+import { FC, useLayoutEffect, useState } from 'react';
+import { collectionLimits } from '../../globals/constants';
+import { Theme, Validated, validatedDefault } from '../../types/global';
+import { SelectTheme } from '../SelectTheme/SelectTheme';
+import { ValidatedTextField } from '../ValidatedTextField/ValidatedTextField';
+
+interface CreateCollectionModalProps {
+    open: boolean;
+    onClose: () => void;
+}
+
+const getDefaultForm = () => ({
+    title: validatedDefault<string | null>(''),
+    theme: validatedDefault<Theme | null>(null),
+});
+
+export const CreateCollectionModal: FC<CreateCollectionModalProps> = (props) => {
+    const [form, setForm] = useState(getDefaultForm());
+
+    const isValid = (): boolean => Object.values(form).some((v) => v.error);
+
+    const onCreate = () => {
+        if (!isValid()) {
+            return;
+        }
+        console.log('ok');
+    };
+
+    console.log('form', form);
+    return (
+        <Dialog open={props.open} onClose={props.onClose} maxWidth={'sm'} fullWidth>
+            <DialogTitle>Создание коллекции</DialogTitle>
+            <DialogContent>
+                <SelectTheme value={form.theme} onValueChange={(theme) => setForm({ ...form, theme })} notNullOrEmpty />
+                <ValidatedTextField
+                    value={form.title}
+                    onValueChange={(v) => setForm({ ...form, title: v })}
+                    label="Название"
+                    notNullOrEmpty
+                    maxLength={collectionLimits.titleMaxLength}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={onCreate}>Создать</Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
