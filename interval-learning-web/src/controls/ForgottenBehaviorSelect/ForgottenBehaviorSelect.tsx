9a9b5e6358ec34296c5d919872698a70ea9e4474
Aleksander Zverev <aleksander.zverev.lt@gmail.com>
2022-03-23 14:47:01 +0500

    fix bugs with refs

--- interval-learning-web/src/controls/ForgottenBehaviorSelect/ForgottenBehaviorSelect.tsx
+++ interval-learning-web/src/controls/ForgottenBehaviorSelect/ForgottenBehaviorSelect.tsx
@@ -1,12 +1,23 @@
 import { MenuItem, Select, SelectProps } from '@mui/material';
 import { FC } from 'react';
+import { useFormContext } from 'react-hook-form';
 import { ForgottenBehavior } from '../../types/schedule';
 
-interface ForgottenBehaviorSelectProps extends SelectProps {}
+interface ForgottenBehaviorSelectProps extends SelectProps {
+    registerName: string;
+}
 
-export const ForgottenBehaviorSelect: FC<ForgottenBehaviorSelectProps> = ({ ...props }) => {
+export const ForgottenBehaviorSelect: FC<ForgottenBehaviorSelectProps> = ({ registerName, ...props }) => {
+    const { register } = useFormContext();
+
     return (
-        <Select {...props} sx={{ width: 275 }} size="small" value={ForgottenBehavior.MoveToNextStep}>
+        <Select
+            {...props}
+            sx={{ width: 275 }}
+            size="small"
+            value={ForgottenBehavior.MoveToNextStep}
+            {...register(registerName)}
+        >
             <MenuItem value={ForgottenBehavior.MoveToNextStep}>Перейти на следующий этап</MenuItem>
             <MenuItem value={ForgottenBehavior.MoveToPreviousStep}>Перейти на предыдущий этап</MenuItem>
             <MenuItem value={ForgottenBehavior.StartFromFirstStep}>Перейти на первый этап</MenuItem>
--- interval-learning-web/src/controls/Modals/CreateScheduleModal.tsx
+++ interval-learning-web/src/controls/Modals/CreateScheduleModal.tsx
@@ -12,18 +12,39 @@ import {
     Typography,
 } from '@mui/material';
 import { FC, useState } from 'react';
-import { SubmitHandler, useFieldArray, useForm } from 'react-hook-form';
+import { FormProvider, SubmitHandler, useFieldArray, useForm } from 'react-hook-form';
 import * as yup from 'yup';
-import { ForgottenBehavior } from '../../types/schedule';
+import { CreateScheduleItem, ForgottenBehavior, PhaseInfo } from '../../types/schedule';
 import { ForgottenBehaviorSelect } from '../ForgottenBehaviorSelect/ForgottenBehaviorSelect';
 import { Form, FormField } from '../Form/Form';
 
 enum DurationType {
     Seconds = 1,
     Minutes = 2,
-    Days = 3,
-    Months = 4,
-    Year = 5,
+    Hours = 3,
+    Days = 4,
+}
+
+function DurationTypeToSeconds(value: number, type: DurationType) {
+    let result = value;
+    let resultType = type;
+
+    if (type === DurationType.Days) {
+        result *= 24;
+        type--;
+    }
+
+    if (type === DurationType.Hours) {
+        result *= 60;
+        type--;
+    }
+
+    if (type === DurationType.Minutes) {
+        result *= 60;
+        type--;
+    }
+
+    return result;
 }
 
 interface IPhaseForm {
@@ -64,98 +85,115 @@ interface CreateScheduleModalProps {
 }
 
 export const CreateScheduleModal: FC<CreateScheduleModalProps> = (props) => {
+    const formMethods = useForm<IForm>({ resolver: yupResolver(schema) });
     const {
         register,
         handleSubmit,
         control,
         formState: { errors },
-    } = useForm<IForm>({ resolver: yupResolver(schema) });
+    } = formMethods;
 
     const { fields, append } = useFieldArray({ control, name: 'phases' });
 
     const onSubmit: SubmitHandler<IForm> = (data) => {
-        console.log('form', data);
+        const schedule: CreateScheduleItem = {
+            cardsCountPerPhase: data.cardsCountPerPhase,
+            forgottenBehavior: data.forgottenBehavior,
+            title: data.title,
+            description: data.description,
+            phases: data.phases.map((p) => {
+                const seconds = DurationTypeToSeconds(p.durationFromLastPhase, p.durationType);
+                const phase: PhaseInfo = {
+                    secondsFromLastPhase: seconds,
+                    description: p.description,
+                };
+
+                return phase;
+            }),
+        };
+
+        console.log('schedule', schedule);
     };
 
     return (
         <Dialog open={props.open} onClose={props.onClose} maxWidth={'sm'} fullWidth>
             <DialogTitle>Создание учебного плана</DialogTitle>
             <DialogContent>
-                <Form onSubmit={handleSubmit(onSubmit)}>
-                    <FormField label="Название" htmlFor="title-input">
-                        <TextField
-                            id="title-input"
-                            size="small"
-                            fullWidth
-                            error={!!errors.title}
-                            helperText={errors.title?.message || ' '}
-                            {...register('title')}
-                        />
-                    </FormField>
-                    <FormField label="Кол-во карт" htmlFor="card-number">
-                        <TextField
-                            id="card-number"
-                            type="number"
-                            size="small"
-                            error={!!errors.cardsCountPerPhase}
-                            helperText={errors.cardsCountPerPhase?.message || ' '}
-                            {...register('cardsCountPerPhase')}
-                        />
-                    </FormField>
-                    <FormField label="Описание" htmlFor="desc">
-                        <TextField
-                            id="desc"
-                            size="small"
-                            error={!!errors.description}
-                            helperText={errors.description?.message || ' '}
-                            {...register('description')}
-                        />
-                    </FormField>
-                    <FormField label="Действие при забывании" htmlFor="title-input">
-                        <ForgottenBehaviorSelect {...register('forgottenBehavior')} />
-                    </FormField>
-                    <div style={{ marginTop: 20, display: 'flex', flexDirection: 'column' }}>
-                        {fields.map((f, i) => (
-                            <div key={f.id}>
-                                <Typography variant="h6">Phase {i + 1}</Typography>
-                                <FormField label={'Секунд с прошлого этапа'} htmlFor={'input-desc-' + i}>
-                                    <TextField
-                                        id={'input-desc-' + i}
-                                        type={'number'}
-                                        size="small"
-                                        error={!!errors.phases?.at(i)?.durationFromLastPhase}
-                                        helperText={errors.phases?.at(i)?.durationFromLastPhase?.message || ' '}
-                                        {...register(`phases.${i}.durationFromLastPhase`)}
-                                    />
-                                    <Select
-                                        defaultValue={defaultDuration}
-                                        size="small"
-                                        sx={{ width: 85 }}
-                                        {...register(`phases.${i}.durationType`)}
-                                    >
-                                        <MenuItem value={DurationType.Seconds}>Сек</MenuItem>
-                                        <MenuItem value={DurationType.Minutes}>Мин</MenuItem>
-                                        <MenuItem value={DurationType.Days}>Дн</MenuItem>
-                                        <MenuItem value={DurationType.Months}>Мес</MenuItem>
-                                        <MenuItem value={DurationType.Year}>Год</MenuItem>
-                                    </Select>
-                                </FormField>
-                                <FormField label={'Описание'} htmlFor={'desc-' + i}>
-                                    <TextField
-                                        id={'desc-' + i}
-                                        type={'number'}
-                                        size="small"
-                                        error={!!errors.phases?.at(i)?.description}
-                                        helperText={errors.phases?.at(i)?.description?.message || ' '}
-                                        {...register(`phases.${i}.description`)}
-                                    />
-                                </FormField>
-                            </div>
-                        ))}
-                    </div>
-                    <Button onClick={() => append({ durationFromLastPhase: 0, description: '' })}>Add</Button>
-                    <Button type="submit">Создать</Button>
-                </Form>
+                <FormProvider {...formMethods}>
+                    <Form onSubmit={handleSubmit(onSubmit)}>
+                        <FormField label="Название" htmlFor="title-input">
+                            <TextField
+                                id="title-input"
+                                size="small"
+                                fullWidth
+                                error={!!errors.title}
+                                helperText={errors.title?.message || ' '}
+                                {...register('title')}
+                            />
+                        </FormField>
+                        <FormField label="Кол-во карт" htmlFor="card-number">
+                            <TextField
+                                id="card-number"
+                                type="number"
+                                size="small"
+                                error={!!errors.cardsCountPerPhase}
+                                helperText={errors.cardsCountPerPhase?.message || ' '}
+                                {...register('cardsCountPerPhase')}
+                            />
+                        </FormField>
+                        <FormField label="Описание" htmlFor="desc">
+                            <TextField
+                                id="desc"
+                                size="small"
+                                error={!!errors.description}
+                                helperText={errors.description?.message || ' '}
+                                {...register('description')}
+                            />
+                        </FormField>
+                        <FormField label="Действие при забывании" htmlFor="title-input">
+                            <ForgottenBehaviorSelect registerName="forgottenBehavior" />
+                        </FormField>
+                        <div style={{ marginTop: 20, display: 'flex', flexDirection: 'column' }}>
+                            {fields.map((f, i) => (
+                                <div key={f.id}>
+                                    <Typography variant="h6">Phase {i + 1}</Typography>
+                                    <FormField label={'Секунд с прошлого этапа'} htmlFor={'input-desc-' + i}>
+                                        <TextField
+                                            id={'input-desc-' + i}
+                                            type={'number'}
+                                            size="small"
+                                            error={!!errors.phases?.at(i)?.durationFromLastPhase}
+                                            helperText={errors.phases?.at(i)?.durationFromLastPhase?.message || ' '}
+                                            {...register(`phases.${i}.durationFromLastPhase`)}
+                                        />
+                                        <Select
+                                            defaultValue={defaultDuration}
+                                            size="small"
+                                            sx={{ width: 85 }}
+                                            {...register(`phases.${i}.durationType`)}
+                                        >
+                                            <MenuItem value={DurationType.Seconds}>Сек</MenuItem>
+                                            <MenuItem value={DurationType.Minutes}>Мин</MenuItem>
+                                            <MenuItem value={DurationType.Hours}>Час</MenuItem>
+                                            <MenuItem value={DurationType.Days}>Дн</MenuItem>
+                                        </Select>
+                                    </FormField>
+                                    <FormField label={'Описание'} htmlFor={'desc-' + i}>
+                                        <TextField
+                                            id={'desc-' + i}
+                                            size="small"
+                                            error={!!errors.phases?.at(i)?.description}
+                                            helperText={errors.phases?.at(i)?.description?.message || ' '}
+                                            {...register(`phases.${i}.description`)}
+                                        />
+                                    </FormField>
+                                </div>
+                            ))}
+                        </div>
+                        <Button onClick={() => append({ durationFromLastPhase: 0, description: '' })}>Add</Button>
+                        <Button type="submit">Создать</Button>
+                    </Form>
+                </FormProvider>
             </DialogContent>
             <DialogActions></DialogActions>
         </Dialog>
