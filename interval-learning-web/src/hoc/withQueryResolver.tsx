bd95837ab8ccacc5e4641b9b1c4156c4159d0570
Aleksander Zverev <aleksander.zverev.lt@gmail.com>
2022-04-30 10:44:08 +0500

    add withMutationResolver

--- interval-learning-web/src/hoc/withQueryResolver.tsx
+++ interval-learning-web/src/hoc/withQueryResolver.tsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/display-name */
-import React from 'react';
-import { QueryDefinition } from '@reduxjs/toolkit/dist/query';
-import { UseQuery } from '@reduxjs/toolkit/dist/query/react/buildHooks';
+import React, { useRef, useState } from 'react';
+import { MutationDefinition, QueryDefinition } from '@reduxjs/toolkit/dist/query';
+import { UseMutation, UseQuery } from '@reduxjs/toolkit/dist/query/react/buildHooks';
 import { TagType } from '../redux/apiSlice';
-import { CircularProgress } from '@mui/material';
+import { CircularProgress, Portal } from '@mui/material';
 import { CustomBaseQueryType } from '../redux/axiosBaseQuery';
+import { CenterContainer } from '../controls/CenterContainer/CenterContainer';
+import { ModalLoader } from '../ModalLoader/ModalLoader';
+import { AssertionModal } from '../controls/Modals/AssertionModal';
 
 interface ResolverProps<TQueryArg> {
     containsFetching?: boolean;
@@ -37,7 +40,11 @@ export const withQueryResolver =
 
         if (isFetching) {
             console.debug('loading');
-            return <CircularProgress />;
+            return (
+                <CenterContainer>
+                    <CircularProgress />
+                </CenterContainer>
+            );
         }
 
         if (isError || !data) {
@@ -56,7 +63,6 @@ export const withOtherQueryResolver =
     <TComponentProps, TOtherQueryArg>(
         Component: React.FunctionComponent<ResolverProps<TOtherQueryArg> & TComponentProps>
     ) =>
-    // eslint-disable-next-line react/display-name
     (props: { queryArg: TQueryArg & TOtherQueryArg } & TComponentProps) => {
         const { queryArg, ...otherProps } = props;
         const { data, isError, isFetching, isSuccess, error } = useQuery(queryArg);
@@ -68,5 +74,88 @@ export const withOtherQueryResolver =
                 containsError={isError || !isSuccess}
                 {...props}
             />
+        );
+    };
+
+type GetInnerType<S> = S extends UseMutation<infer T> ? T : never;
+
+type GetArgs<S> = S extends UseMutation<infer TDefinition>
+    ? TDefinition extends MutationDefinition<infer TArg, CustomBaseQueryType, TagType, any>
+        ? TArg
+        : never
+    : never;
+
+type GetResult<S> = S extends UseMutation<infer TDefinition>
+    ? TDefinition extends MutationDefinition<any, CustomBaseQueryType, TagType, infer TResult>
+        ? TResult
+        : never
+    : never;
+
+export interface WithMutationResolverProps<
+    T extends UseMutation<MutationDefinition<any, CustomBaseQueryType, TagType, any>>
+> {
+    mutate: (args: GetArgs<T>) => Promise<GetResult<T>>;
+    showRetryModal: (retry: () => void) => void;
+    isLoading: boolean;
+    isSuccess: boolean;
+    mutationData: GetResult<T> | undefined;
+}
+
+interface MutationResolverProps {}
+
+export const withMutationResolver =
+    <TMutationArg, TResult>(
+        useMutation: UseMutation<MutationDefinition<TMutationArg, CustomBaseQueryType, TagType, TResult>>,
+        errorMessage: string
+    ) =>
+    <TComponentProps,>(
+        Component: React.FunctionComponent<WithMutationResolverProps<typeof useMutation> & TComponentProps>
+    ) =>
+    (props: MutationResolverProps & Omit<TComponentProps, keyof WithMutationResolverProps<typeof useMutation>>) => {
+        const [mutate, mutationArgs] = useMutation();
+        const { data, isLoading, isError, isSuccess, originalArgs } = mutationArgs;
+
+        const onMutate = (args: TMutationArg) => mutate(args).unwrap();
+
+        const { ...otherProps } = props;
+        const [showAssertionModal, setShowAssertionModal] = useState(false);
+        const { current } = useRef<{ retryFunc: (() => void) | null }>({ retryFunc: null });
+
+        const onRetry = () => {
+            current.retryFunc && current.retryFunc();
+            setShowAssertionModal(false);
+        };
+
+        const showRetryModal = (retry: () => void) => {
+            setShowAssertionModal(true);
+            current.retryFunc = retry;
+        };
+
+        return (
+            <>
+                <Portal>
+                    {showAssertionModal && (
+                        <AssertionModal
+                            open
+                            onClose={() => setShowAssertionModal(false)}
+                            onCancel={() => setShowAssertionModal(false)}
+                            onAssert={onRetry}
+                            title={'Ошибка при отправке данных'}
+                            message={errorMessage}
+                            assertTitle={'Повторить запрос'}
+                            cancelTitle={'Отмена'}
+                        />
+                    )}
+                </Portal>
+                <ModalLoader loading={isLoading} />
+                <Component
+                    mutate={onMutate}
+                    showRetryModal={showRetryModal}
+                    isLoading={isLoading}
+                    isSuccess={isSuccess}
+                    mutationData={data}
+                    {...otherProps}
+                />
+            </>
         );
     };
