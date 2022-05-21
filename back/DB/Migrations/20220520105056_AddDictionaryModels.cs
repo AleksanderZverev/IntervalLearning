21bb41ab7fc0af4f09dbbd43193513e69782ee40
Aleksander Zverev <aleksander.zverev.lt@gmail.com>
2022-05-21 12:25:12 +0500

    Dictionary models migration

---
+++ back/DB/Migrations/20220520105056_AddDictionaryModels.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DB.Migrations
+{
+    public partial class AddDictionaryModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<short>(
+                name: "LanguageId",
+                table: "Themes",
+                type: "smallint",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Languages",
+                columns: table => new
+                {
+                    Id = table.Column<short>(type: "smallint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Languages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Words",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Word = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Pronunciation = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    LanguageId = table.Column<short>(type: "smallint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Words", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Words_Languages_LanguageId",
+                        column: x => x.LanguageId,
+                        principalTable: "Languages",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Themes_LanguageId",
+                table: "Themes",
+                column: "LanguageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Words_LanguageId",
+                table: "Words",
+                column: "LanguageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Themes_Languages_LanguageId",
+                table: "Themes",
+                column: "LanguageId",
+                principalTable: "Languages",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Themes_Languages_LanguageId",
+                table: "Themes");
+
+            migrationBuilder.DropTable(
+                name: "Words");
+
+            migrationBuilder.DropTable(
+                name: "Languages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Themes_LanguageId",
+                table: "Themes");
+
+            migrationBuilder.DropColumn(
+                name: "LanguageId",
+                table: "Themes");
+        }
+    }
+}
