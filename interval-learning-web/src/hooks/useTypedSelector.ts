8ad7a031a3d475e04fb746d966bf3475b3240dc6
Aleksander Zverev <aleksander.zverev.lt@gmail.com>
2022-06-04 15:05:27 +0500

    useRequiredTypedSelector

--- interval-learning-web/src/hooks/useTypedSelector.ts
+++ interval-learning-web/src/hooks/useTypedSelector.ts
@@ -3,4 +3,12 @@ import { RootState } from '../redux/store';
 
 const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
 
+export const useRequiredTypedSelector = <TResult>(
+    select: (state: RootState) => TResult | undefined | null
+): TResult => {
+    const result = useTypedSelector(select);
+    if (!result) throw new Error('Required service is undefined');
+    return result;
+};
+
 export default useTypedSelector;
