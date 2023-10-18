030ed3f0a9f9be5b123bd5fb89f1b10f45299511
Aleksandr Zverev <alt.zve@gmail.com>
2023-10-18 16:11:52 +0500

    Hide console window of vs code

--- open-dev-env.ps1
+++ open-dev-env.ps1
@@ -1,4 +1,4 @@
 Start-Process -FilePath "gitex" -WorkingDirectory "." -WindowStyle Maximized
 Start-Process -FilePath "./back/IntervalLearningApi.sln" -WindowStyle Maximized
-Start-Process -FilePath "code" -WorkingDirectory "." -ArgumentList "--new-window", "./interval-learning-web"
+Start-Process code -WorkingDirectory "." -ArgumentList "--new-window", "./interval-learning-web" -WindowStyle Hidden
 Start-Process powershell.exe -ArgumentList "-f", "./dev.ps1" -WorkingDirectory "./interval-learning-web"
