3b58ab0acf60df6888bb9bfd89a4934739f6f488
Aleksandr Zverev <alt.zve@gmail.com>
2024-04-29 15:56:40 +0500

    RememberAnswer class

--- interval-learning-web/src/types/global.ts
+++ interval-learning-web/src/types/global.ts
@@ -3,3 +3,16 @@ export interface Theme {
     name: string;
     languageId: string | null;
 }
+
+export class RememberAnswer {
+    private weight: number;
+
+    constructor(weight: number) {
+        this.weight = weight;
+    }
+
+    public GetWeight = () => this.weight;
+    public IsRemembered = () => this.weight != null && this.weight >= 0.8;
+    public IsNotSure = () => this.weight != null && this.weight >= 0.4 && this.weight < 0.8;
+    public IsForgotten = () => this.weight != null && this.weight < 0.4;
+}
